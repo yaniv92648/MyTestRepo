f2d029418b34d4fa7bbe1e87a717ba151e20290e
yaniv92648 <yaniv92648@gmail.com>
2018-04-30 14:29:45 +0300

    Init

---
+++ MyTestApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyTestApp
+//
+//  Created by Yaniv Cohen on 30/04/2018.
+//  Copyright © 2018 Yaniv Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Test
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
